de0302bb9460aada856cc9ed05ffe1dfc0244943
ashutosh-1806 <ashutosh.tripathi@vance.tech>
2024-08-13 02:31:03 +0530

    minor fix

--- dashboards.tf
+++ dashboards.tf
@@ -1,6 +1,6 @@
 resource "grafana_dashboard" "aws" {
   provider    = grafana.cloud
-  for_each    = fileset("${path.module}/dashboards/aws", "*.json")
-  config_json = file("${path.module}/dashboards/aws/${each.key}")
+  for_each    = fileset("${path.module}/Dashboards/aws", "*.json")
+  config_json = file("${path.module}/Dashboards/aws/${each.key}")
   folder      = grafana_folder.AWS.id
 }
