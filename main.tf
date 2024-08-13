c8038c99b4f874ebaffecb0f6ddf0c77c8622395
ashutosh-1806 <ashutosh.tripathi@vance.tech>
2024-08-13 10:48:45 +0530

    added alerts

--- main.tf
+++ main.tf
@@ -10,5 +10,5 @@ terraform {
 provider "grafana" {
   alias = "cloud"
   url   = "https://ashutoshtripathi1806.grafana.net"
-  auth  = "glsa_KW9gZpB1UTn9n1MGjcuvWJ8CFI3BzAH1_8cb59580"
+  auth  = ""
 }
